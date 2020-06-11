555f5829381076e8a8892bb2b09c1116a46a7430
bmcart3 <bmcart3@gmail.com>
2020-06-11 16:29:36 -0400

    selectall is working but the post is not

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,42 +4,43 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-    burger.selectAll(function(data) {
-      var hbsObject = {
-        burgers: data
-      };
-      console.log(hbsObject);
-      res.render("index", hbsObject);
-    });
+router.get("/", function (req, res) {
+  burger.selectAll(function (data) {
+    var hbsObject = {
+      burgers: data
+    };
+    //console.log(hbsObject);
+    res.render("index", hbsObject);
   });
-  
-  router.post("/api/burgers", function(req, res) {
-    //console.log(req.body);
-    burger.insertOne(["burger_name"], req.body.burger_name, function(result) {
-      // Send back the ID of the new quote
-      res.json({ id: result.insertId });
-    });
+});
+
+router.post("/api/burgers", function (req, res) {
+  console.log(req.body.burger_name);
+  burger.insertOne("burger_name", req.body.burger, function (result) {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId }); //does it need to be res.redirect({"/"});
+    
   });
-  
-  router.put("/api/burgers/:id", function(req, res) {
-    //console.log(req.body.devoured);
-    var condition = "id = " + req.params.id;
-    //console.log(condition);
-  
-    console.log("condition", condition);
-  
-    burger.updateOne({
-      devoured: req.body.devoured
-    }, condition, function(result) {
-      if (result.changedRows == 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      } else {
-        res.status(200).end();
-      }
-    });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+  //console.log(req.body.devoured);
+  var condition = "id = " + req.params.id;
+  //console.log(condition);
+
+  //console.log("condition", condition);
+
+  burger.updateOne({
+    devoured: req.body.devoured
+  }, condition, function (result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
   });
+});
 
 
 

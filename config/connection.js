33d584953d46f09127c9cb241e78c7460c953760
bmcart3 <bmcart3@gmail.com>
2020-06-11 19:21:24 -0400

    updating connection for deployment

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,28 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "rootroot",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "rootroot",
+    database: "burgers_db"
+  });
+};
 
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "rootroot",
+//   database: "burgers_db"
+// });
 // Make connection.
-connection.connect(function(err) {
+
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
@@ -20,5 +32,8 @@ connection.connect(function(err) {
 
 // Export connection for our ORM to use.
 module.exports = connection;
+
+
+
 
 
